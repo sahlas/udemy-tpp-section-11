280efe9e54f2de94bba03f0fbf92efb90ff78892
bsahlas <bill.sahlas@gmail.com`>
2025-04-29 15:12:10 -0400

    add setup.py changes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import wheel
 
 setup(
-    name="packaging-demo",
+    name="packaging_demo",
     version="0.0.0",
     packages=find_packages(),
     author="Bill Sahlas",

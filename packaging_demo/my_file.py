ed2ee6a29e444357e79d758d9a2eafb19e45fd47
bsahlas <bill.sahlas@gmail.com`>
2025-05-01 12:57:21 -0400

    Section 12 - delete unused module.

--- packaging_demo/my_file.py
+++
@@ -1,9 +0,0 @@
-# from my_folder import A
-import numpy as np
-
-from packaging_demo._my_different_file import GREATINGS as CONSTANT
-
-MY_ARRAY = np.array([1, 2])
-print(MY_ARRAY)
-
-print(CONSTANT)
